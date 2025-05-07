693aa0520708fed637dd03d86d731a0e512827ba
AMIRKATE <amirkateb36@gmail.com>
2025-05-07 15:58:39 +0330

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+chat_history = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    emit('chat history', chat_history)
+    emit('message', {'user': 'Server', 'msg': f'{username} has joined the chat'}, broadcast=True)
+
+@socketio.on('message')
+def handle_message(data):
+    chat_entry = {'user': data['user'], 'msg': data['msg']}
+    chat_history.append(chat_entry)
+    emit('message', chat_entry, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
